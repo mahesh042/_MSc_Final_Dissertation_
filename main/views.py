342333d85a13511425cc2218aaa52318bf66331b
Mahesh_Srivinay <rmsvinay123@gmail.com>
2022-04-05 13:28:46 +0100

    commit blank

--- main/views.py
+++ main/views.py
@@ -13,8 +13,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 import random, json
-from .models import DockStation
-from .models import UserOTP
+from .models import DockStation,UserOTP
 from django.core.mail import send_mail
 from django.contrib import messages
 # Create your views here.
